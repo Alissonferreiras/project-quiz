a930e90ef90d17619f065aeda632127c1ae8d9bc
Alissonferreiras <alissonferreira.99@outlook.com>
2023-07-26 19:56:57 -0300

    feat: add button

---
+++ src/components/button/index.jsx
@@ -0,0 +1,7 @@
+import S from './style.module.css'
+
+export function Button (props) {
+    return (
+        <button className={S.container} onClick={props.onClick}>{props.children}</button>
+    )
+}
